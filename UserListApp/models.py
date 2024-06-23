14eee09c40b486a6f75d818ece1bc7aaa0966c93
JP Cadinha <jpcadinha@gmail.com>
2024-06-23 23:53:27 +0100

    Created the User model

--- UserListApp/models.py
+++ UserListApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user = models.CharField(max_length=250)
+    pub_date = models.DateTimeField("Date published")
+
+    def __str__(self):
+        return self.user
